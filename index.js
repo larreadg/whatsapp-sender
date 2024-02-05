1a9805b60f44b4172206aade4a6c327f5660eee3
Diego Larrea <larreadi@personal.com.py>
2024-02-05 09:05:14 -0300

    FIX: Se agrega envio de mensajes con pdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const qrcode = require('qrcode-terminal');
-const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
 const client = new Client({authStrategy: new LocalAuth()});
@@ -10,24 +11,32 @@ client.on('qr', (qr) => {
 
 client.initialize();
 
-
-
 client.on('ready', async () => {
-    console.log('Client is ready!');
     
-    const recipients = ['595984345259@c.us', '595972413798@c.us'];
-    const url = 'https://natalia-miguel.love';
-    const previewData = await fetch(`https://api.linkpreview.net/?key=f6c04bcd02c17b05e311b2e094649db8&q=${url}`);
-    const previewJson = await previewData.json();
+    let data = JSON.parse(fs.readFileSync('data.json', { encoding : 'utf-8' }))
+    const recipients = data.map(el => {
+        el.celular = `${el.celular}@c.us`
+        return el
+    })
+    
+    const pdfPath = path.join('C:', 'Users', 'larreadi', 'Downloads', 'Boda Natalia & Miguel.pdf');
+    const pdfData = fs.readFileSync(pdfPath);
+    const base64Pdf = pdfData.toString('base64');
 
-    const response = await fetch(previewJson.image);
-    const buffer = await response.buffer();
-    const base64Image = buffer.toString('base64');
-
-    const media = new MessageMedia('image/webp', base64Image, previewJson.title);
+    const media = new MessageMedia('application/pdf', base64Pdf, 'Boda Natalia & Miguel.pdf');
 
     for(let i = 0; i < recipients.length; i++) {
-        await client.sendMessage(recipients[i], media, { caption: previewJson.description });
-        console.log(`Mensaje enviado a ${recipients[i]}`)
+        const message = `Hola ${recipients[i].nombre}!\n\nCon mucha alegría queremos invitarte a nuestra boda. Queremos compartir este día tan especial para nosotros rodeados de amigos y familiares.\n\nAccediendo al siguiente link vas a poder ver la invitación completa y confirmar tu asistencia: https://natalia-miguel.love/\n\n¡Esperamos contar con su presencia para celebrar juntos! 💕\n\nVálido ${recipients[i].valido} persona(s) Confirmar asistencia hasta el 11 de febrero por el enlace\n\nSi te gustaría hacernos un regalo te dejamos algunas opciones en el siguiente enlace: https://www.tupi.com.py/regalos_en_lista/6836/Boda-de-Natalia-Cardozo-y-Miguel-Gonzalez-el-24-02-2024`;
+        await client.sendMessage(recipients[i].celular, media, { caption: message });
+        console.log(`Mensaje enviado a ${recipients[i].nombre}`);
+        await esperar2m();
     }
 });
+
+const esperar2m = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('Han pasado 2 minutos');
+        }, 2 * 60 * 1000); // Espera 2 minutos
+    });
+}
